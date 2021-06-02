3502a72e41e7934fd0d03237ea515dbc55ba198f
Juan Palacios <juan980424@gmail.com>
2021-06-02 09:10:21 -0500

    test in functions async-await

--- src/base/11-async-await.js
+++ src/base/11-async-await.js
@@ -1,28 +1,20 @@
-
-
 // const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
 // getImagenPromesa().then( console.log );
 
-export const getImagen = async() => {
+export const getImagen = async () => {
+  try {
+    const apiKey = "Ajj6vRICJOOW7Zhl8f1ILnaXoTz83bqK";
+    const resp = await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+    );
+    const { data } = await resp.json();
 
-    try {
+    const { url } = data.images.original;
 
-        const apiKey = 'LLcsfFYpM7fRcDNI89DMWGetu8BjhfeZ';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
+    return url;
+  } catch (error) {
+    return 'No existe'
+  }
+};
 
-        const { url } = data.images.original;
-
-        return url
-    } catch (error) {
-        return 'dont exist'
-    }
-    
-    
-    
-}
-
- getImagen();
-
-
-
+getImagen();
--- src/test/base/11-async-await.test.js
+++ src/test/base/11-async-await.test.js
@@ -1,3 +1,11 @@
-describe('test in async funtions with async and await', () => {
+import {getImagen} from '../../base/11-async-await'
+
+describe('test with async functions and fetch', () => {
+  test('should return a url from image', async () => {
+    const url = await getImagen();
+    console.log(url)
+    expect(url.includes('https://')).toBe(true);
+
+  })
   
 })
