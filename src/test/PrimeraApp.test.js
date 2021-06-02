bad618142e24a5fc67601addda18c192f1b726cd
Juan Palacios <juan980424@gmail.com>
2021-06-02 10:11:39 -0500

    test in primeraApp component using enzyme

--- src/test/PrimeraApp.test.js
+++ src/test/PrimeraApp.test.js
@@ -1,13 +1,23 @@
-import { render } from "@testing-library/react"
+// import { render } from "@testing-library/react"
+
+import {shallow} from 'enzyme'
 import PrimeraApp from "../PrimeraApp"
 
 describe('test in <PrimeraApp />', () => {
-  test('should look message "hola soy juan"', () => {
-    const saludo = 'hola soy juan'
 
-    const wrapper = render(<PrimeraApp saludo ={saludo}/>)
+  // test('should look message "hola soy juan"', () => {
+  //   const saludo = 'hola soy juan'
 
-    expect(wrapper.getByText(saludo)).toBeInTheDocument();
+  //   const wrapper = render(<PrimeraApp saludo ={saludo}/>)
+
+  //   expect(wrapper.getByText(saludo)).toBeInTheDocument();
+  // })
+  
+  test('should look the component <PrimeraApp /> correctly', () => {
+
+    const saludo = 'Hola soy Juan'
+    const wrapper = shallow(<PrimeraApp  saludo={saludo}/>)
+    expect(wrapper).toMatchSnapshot();
   })
   
 })
